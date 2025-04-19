87cc9f225473b079c9a62e7f94a2cc55b1a0070c
Vitor Lopes <vitorlopesmed@gmail.com>
2025-04-19 17:36:13 +0100

    test file add to search bar

--- crypto-dashboard/src/components/SearchBar.jsx
+++ crypto-dashboard/src/components/SearchBar.jsx
@@ -5,7 +5,6 @@ function SearchBar({ searchTerm, onSearchChange }) {
     return (
         <div>
             <label htmlFor="search" className={styles.searchLabel}>
-                Search
             </label>
             <input input="search" type ="text" placeholder="Search coin..." className={styles.searchInput} value={searchTerm} onChange={(e) => onSearchChange(e.target.value)}></input>
         </div>
