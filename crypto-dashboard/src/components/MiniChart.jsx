d57ce9b2e4dd49196ac4b474f03fd402121f6085
Vitor Lopes <vitorlopesmed@gmail.com>
2025-04-19 17:43:30 +0100

    import and register parts of chart.js

--- crypto-dashboard/src/components/MiniChart.jsx
+++ crypto-dashboard/src/components/MiniChart.jsx
@@ -1,2 +1,25 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
+
+//Necessary pieces from chart.js
+
+import {
+    Chart as ChartJS,
+    LineElement,
+    PointElement,
+    LinearScale,
+    CategoryScale,
+    Tooltip,
+    Legend
+} from 'chart.js';
+
+//Register pieces
+
+ChartJS.register(
+    LineElement,
+    PointElement,
+    LinearScale,
+    CategoryScale,
+    Tooltip,
+    Legend
+  );
