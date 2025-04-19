0b50482063f09b11f0c7b93ff9ead3558fd99c17
Vitor Lopes <vitorlopesmed@gmail.com>
2025-04-19 16:13:25 +0100

    Create coins state variable inside home component

--- crypto-dashboard/src/app/page.js
+++ crypto-dashboard/src/app/page.js
@@ -5,5 +5,12 @@ import { useEffect, useState } from 'react';
 import { getTopCoins } from '@/utils/api';
 
 export default function Home() {
-  return <h1>Crypto Dashboard</h1>
+
+  const [coins, setCoins] = useState([]);
+
+  return (
+    <div>
+      <h1>Crypto Dashboard</h1>
+    </div>
+  );
 }
