165ce25f0269e5ded186c255891bd7a4956501e2
Vitor Lopes <vitorlopesmed@gmail.com>
2025-04-19 17:33:06 +0100

    Now is possible to search coin with symbol

--- crypto-dashboard/src/app/page.js
+++ crypto-dashboard/src/app/page.js
@@ -12,9 +12,12 @@ export default function Home() {
   //loading state for waiting data
   const [loading, setLoading] = useState(true);
   const [searchTerm, setSearchTerm] = useState('');
-  const filteredCoins = coins.filter((coin) =>
-  coin.name.toLowerCase().includes(searchTerm.toLowerCase())
-);
+  const filteredCoins = coins.filter((coin) => {
+    const term = searchTerm.toLowerCase();
+    return (
+      coin.name.toLowerCase().includes(term) || coin.symbol.toLowerCase().includes(term)
+    );
+  });
 
   useEffect(() => {
     async function fetchCoins() {
