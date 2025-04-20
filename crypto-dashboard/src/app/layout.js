e7de2732886300ee49b50cc0224bcf5efbaabf6b
Vitor Lopes <vitorlopesmed@gmail.com>
2025-04-20 20:24:38 +0100

    Roboto font add to layout.s

--- crypto-dashboard/src/app/layout.js
+++ crypto-dashboard/src/app/layout.js
@@ -1,3 +1,7 @@
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
