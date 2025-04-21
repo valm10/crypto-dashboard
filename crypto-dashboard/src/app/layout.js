0e85286bdde4356e5e023fa0ed7892fc4104353b
Vitor Lopes <vitorlopesmed@gmail.com>
2025-04-21 19:49:41 +0100

    install libraries for testing (Jest + React Testing Library )

--- crypto-dashboard/src/app/layout.js
+++ crypto-dashboard/src/app/layout.js
@@ -1,7 +1,6 @@
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
