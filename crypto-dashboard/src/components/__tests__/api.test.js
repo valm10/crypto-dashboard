09d0ecc4edb8883c2f0a08c18b473003a90a1166
Vitor Lopes <vitorlopesmed@gmail.com>
2025-04-23 16:43:45 +0100

    getTopCoins should return fake coin data from api

--- crypto-dashboard/src/components/__tests__/api.test.js
+++ crypto-dashboard/src/components/__tests__/api.test.js
@@ -1,5 +1,27 @@
 import axios from "axios";
 import { getTopCoins } from "../api";
 
-
 jest.mock("axios");
+//correct case
+test("getTopCoins returns coin data", async () => {
+    const fakeCoins = [{ id: "bitcoin", name: "Bitcoin" }];
+    axios.get.mockResolvedValue({ data: fakeCoins });
+  
+    const data = await getTopCoins();
+  
+    expect(axios.get).toHaveBeenCalledWith(
+      "https://api.coingecko.com/api/v3/coins/markets",
+      {
+        params: {
+          vs_currency: "eur",
+          order: "market_cap_desc",
+          per_page: 10,
+          page: 1,
+          sparkline: true,
+        },
+      }
+    );
+  
+    expect(data).toEqual(fakeCoins);
+  });
+  
