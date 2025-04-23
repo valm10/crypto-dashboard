7ebf4a54c8f6bc419cb967857595c67be9398b71
Vitor Lopes <vitorlopesmed@gmail.com>
2025-04-23 16:33:36 +0100

    coin fake + render

--- crypto-dashboard/src/components/__tests__/CoinCard.test.js
+++ crypto-dashboard/src/components/__tests__/CoinCard.test.js
@@ -4,3 +4,24 @@ import CoinCard from "../CoinCard";
 
 //minichart mock
 jest.mock("../MiniChart", () => () => <div data-testid="mock-chart" />);
+//coin fake + render
+describe("CoinCard component", () => {
+    test("renders coin data correctly", () => {
+      const fakeCoin = {
+        id: "bitcoin",
+        name: "Bitcoin",
+        symbol: "btc",
+        image: "https://example.com/bitcoin.png",
+        current_price: 50000,
+        price_change_percentage_24h: 2.3456,
+        market_cap: 1000000000,
+        total_volume: 300000000,
+        market_cap_rank: 1,
+        circulating_supply: 19000000,
+        sparkline_in_7d: {
+          price: [100, 200, 300, 400, 500]
+        },
+      };
+  
+      render(<CoinCard coin={fakeCoin} />);
+  
