df8cf64abc38cc98532713a9f844c272a6960f3a
Vitor Lopes <vitorlopesmed@gmail.com>
2025-04-23 16:39:34 +0100

    render chart with mock data

--- crypto-dashboard/src/components/__tests__/MiniChart.test.js
+++ crypto-dashboard/src/components/__tests__/MiniChart.test.js
@@ -6,3 +6,12 @@ import MiniChart from "../MiniChart";
 jest.mock("react-chartjs-2", () => ({
   Line: () => <div>Mocked Line Chart</div>,
 }));
+//render test
+test("renders MiniChart with sparkline data", () => {
+    const fakeSparkline = [1, 2, 3, 4, 5];
+  
+    const { getByText } = render(<MiniChart sparkline={fakeSparkline} />);
+  
+    expect(getByText("Mocked Line Chart")).toBeInTheDocument();
+  });
+  
