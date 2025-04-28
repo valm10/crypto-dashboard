00381b7c364d6607f642177168ea57f055f3a06b
Vitor Lopes <vitorlopesmed@gmail.com>
2025-04-28 23:31:02 +0100

    Clean api.js use relative path for API calls, polish comments

--- crypto-dashboard/src/services/api.js
+++ crypto-dashboard/src/services/api.js
@@ -2,28 +2,25 @@ import axios from 'axios';
 
 const BASE_URL = 'https://api.coingecko.com/api/v3';
 
+// Create pre-configured Axios instance
 const api = axios.create({
   baseURL: BASE_URL,
 });
 
 export default api;
 
-//get Top 10 coins by market cap in eur
+// Fetch top 10 coins by market cap in EUR.
 export async function getTopCoins() {
   try {
-    const response = await api.get(
-      'https://api.coingecko.com/api/v3/coins/markets',
-      {
-        params: {
-          vs_currency: 'eur',
-          order: 'market_cap_desc',
-          per_page: 10,
-          page: 1,
-          sparkline: true,
-        },
+    const response = await api.get('/coins/markets', {
+      params: {
+        vs_currency: 'eur',
+        order: 'market_cap_desc',
+        per_page: 10,
+        page: 1,
+        sparkline: true,
       },
-    );
-
+    });
     return response.data;
   } catch (error) {
     console.error('Error fetching top coins:', error);
