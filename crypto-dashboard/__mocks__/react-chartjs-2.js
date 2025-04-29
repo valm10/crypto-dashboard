1e197adcbabd36119787a1152f5f790016a282c4
Vitor Lopes <vitorlopesmed@gmail.com>
2025-04-29 12:54:00 +0100

    added import react to mock file of charts

--- crypto-dashboard/__mocks__/react-chartjs-2.js
+++ crypto-dashboard/__mocks__/react-chartjs-2.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export const Line = () => <div>Mocked Line Chart</div>;
 export const Bar = () => <div>Mocked Bar Chart</div>;
 export const Doughnut = () => <div>Mocked Doughnut Chart</div>;
